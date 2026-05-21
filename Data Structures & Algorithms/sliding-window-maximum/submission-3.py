16482e10e329595949ee1eef0eb0e0823362bf9d
Arthur Vinicius Carneiro Nunes <123214565+ApenasUmSonhador@users.noreply.github.com>
2026-05-21 11:28:27 -0300

    Add: sliding-window-maximum - submission-3

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        l = 0
+        ans = []
+        for r in range(k, len(nums)+1):
+            heap = nums[l:r]
+            heapq.heapify_max(heap)
+            ans.append(heapq.heappop_max(heap))
+            l += 1
+        return ans
